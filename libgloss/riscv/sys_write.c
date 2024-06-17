de0233f49742a666603f4da11a3ea552c37b41c4
Andreas Stergiopoulos <andreas.stergiopoulos@tuni.fi>
2024-06-17 13:58:12 +0300

    Implemented sys_write syscall

--- libgloss/riscv/sys_write.c
+++ libgloss/riscv/sys_write.c
@@ -1,10 +1,15 @@
 #include <machine/syscall.h>
 #include <sys/types.h>
 #include "internal_syscall.h"
+#include "uart8250.h"
 
 /* Write to a file.  */
 ssize_t
 _write(int file, const void *ptr, size_t len)
 {
-  return syscall_errno (SYS_write, file, ptr, len, 0, 0, 0);
+  for(size_t i=0; i < len; i++)
+  {
+    uart8250_putc(((char*)ptr)[i]);
+  }
+  return 0;
 }
